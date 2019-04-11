00fd5280c4f237e3981e5dbd3b76f6032047ec21
your name here <samuelchavez@students.berkeley.net>
2019-04-11 13:16:10 -0700

    jjj

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,10 @@
 public class Runner {
     public static void main(String[] args) {
+        Teacher a = new Teacher("muller" , "Bio" , "")
+
+
         School BHS = new School("BHS");
-        Section math = new Section(  , "Math");
-        Teacher a = new Teacher
-        Section bio = new Section("mueller","biology");
-        Section compsci = new Section("Albinson", "compsci");
+        Section Bio = new Section("Mulller", "")
     }
 
 
--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,38 @@
 public class Student extends Person{
+
+    public Student (String name, int id, int grade ){
+    super(name, id);
+    this.grade = grade;
+}
+    private  Section[] section  = new Section[10];
+    private int sectionCount = 0;
     private int grade;
 
-    public Student (int g, String n){
-        super(n);
-        this.grade = g;
+    public Section[] getSection() {
+        return section;
     }
+
+    public void setSection(Section[] section) {
+        this.section = section;
+    }
+
+    public int getSectionCount() {
+        return sectionCount;
+    }
+
+    public void setSectionCount(int sectionCount) {
+        this.sectionCount = sectionCount;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+
+
+
 }
