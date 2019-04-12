f8f8ac60c3941dac09b43e1989bf1c21993f7a56
your name here <samuelchavez@students.berkeley.net>
2019-04-12 13:22:00 -0700

    jjj

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,27 @@
 public class Runner {
     public static void main(String[] args) {
-        Teacher a = new Teacher("muller" , "Bio" , "")
-
-
+        Teacher a = new Teacher("mueller" , "Bio" , 1);
+        Teacher b = new Teacher("Palen" , "math", 2);
+        Teacher c = new Teacher("Albinson" , "Computer Science" , 3);
         School BHS = new School("BHS");
-        Section Bio = new Section("Mulller", "")
+        Section Bio = new Section(a, "Bio");
+        Section Math = new Section(b , "math");
+        Section Computer_Science = new Section(c , "Computer Science");
+        Student Me = new Student("Sammi" , 1 , 11);
+        Student F1 = new Student("Talia" , 2 , 11);
+        Student F2 = new Student("Manny" , 3 , 11);
+        Student F3 = new Student("Mario" , 4 , 11);
+        Student F4 = new Student("Solia" , 5 , 11);
+        Student F5 = new Student("Rachel" , 6 , 11);
+        Bio.addStudent(Me);
+        Bio.addStudent(F1);
+        Math.addStudent(F2);
+        Math.addStudent(F3);
+        Math.addStudent(F4);
+        Computer_Science.addStudent(F5);
+        System.out.println(Bio.toString());
+        System.out.println(Math.toString());
+        System.out.println(Computer_Science.toString());
     }
 
 
--- src/Section.java
+++ src/Section.java
@@ -6,6 +6,26 @@ public class Section {
 
     }
 
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public void setCurrentSize(int currentSize) {
+        this.currentSize = currentSize;
+    }
+
+    private int currentSize;
+    public Student[] getStudents() {
+        return students;
+    }
+
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    private Student[] students = new  Student[50];
+
     private Teacher teacher;
     private String name;
 
@@ -21,9 +41,23 @@ public class Section {
     public String getName() {
         return name;
     }
+    public void addStudent(Student s){
+             students[currentSize] = s;
+             currentSize++;
+    }
 
     public void setName(String name) {
         this.name = name;
     }
-
+    public String toString(){
+        String n = "";
+        for(int i = 0; i<students.length;i++){
+            if(students[i]!=null) {
+                n = n + " " + students[i].getName();
+            }
+        }
+        String x = "This " + name + " class is taught by " + teacher.getName() + " and has " + currentSize
+                + " students:" + n;
+        return x;
+    }
 }
